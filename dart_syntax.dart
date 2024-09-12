bb388021858b2bf7756d8369202367c601233475
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-09-12 16:08:30 -0400

    first commit

---
+++ dart_syntax.dart
@@ -0,0 +1,7 @@
+main(){
+  var numero = 4; // var se utiliza para declarar cualquier tipo de variable y posteriormente el sistema determina que tipo de dato es segun lo que se defina en la variable
+  num numero2 = 3; // num se utiliza para definir y declarar cualquier tipo de dato numerico
+  int numero3 = 5; // int se utiliza para declarar variables numericas de tipo entero
+  double numero4 = 3.5; // double se utiliza para declarar variables numericas de tipo decimal
+  print("Hola $numero");
+}
